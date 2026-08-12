33dd43415c9c8b4cda8a50da11c36d341384f878
anjaleeyadav <yadavanjali101102@gmail.com>
2026-08-12 20:14:05 +0530

    Initial commit

--- 01-Python-Basics/Practice_Set_2.py
+++ 01-Python-Basics/Practice_Set_2.py
@@ -1,68 +1,71 @@
 # 1. Write a program using functions to find greatest of three numbers.
 
-# def greatest(a =10, b= 20,c=9):
-#     if a>=b and a>=c:
-#         return a
-#     elif b>=a and b>=c :
-#         return b
-#     else:
-#         return c
+def greatest(a =10, b= 20,c=9):
+    if a>=b and a>=c:
+        return a
+    elif b>=a and b>=c :
+        return b
+    else:
+        return c
 
-# print("greatest number is",greatest())
-# # greatest()
-# # greatest(c=74)
+print("greatest number is",greatest()) 
+
+# greatest()
+# greatest(c=74)
 
 
 
-# # 2. Write a python program using function to convert Celsius to Fahrenheit.
+# 2. Write a python program using function to convert Celsius to Fahrenheit.
 
-# def celsius_to_faheren(celsius):
-#     Fahrenheit = (celsius *(9/5)) +32
-#     return Fahrenheit
+def celsius_to_faheren(celsius):
+    Fahrenheit = (celsius *(9/5)) +32
+    return Fahrenheit
 
-# celsius = int(input("Enter celsius :"))
+celsius = int(input("Enter celsius :"))
 
-# result = celsius_to_faheren(celsius)
-# print("Faherenheit is :",result)
+result = celsius_to_faheren(celsius)
+print("Faherenheit is :",result)
 
-# # 3. How do you prevent a python print() function to print a new line at the end.
+# 3. How do you prevent a python print() function to print a new line at the end.
 
-# print("Hello", end = " ")
-# print("World")
+print("Hello", end = " ")
+print("World")
 
-# # 4. Write a recursive function to calculate the sum of first n natural numbers.
+# 4. Write a recursive function to calculate the sum of first n natural numbers.
 
-# def sum_natural(n):
-#     if n ==1:
-#         return 1
-#     else:
-#         return n + sum_natural(n-1)
+def sum_natural(n):
+    if n ==1:
+        return 1
+    else:
+        return n + sum_natural(n-1)
 
-# n = int(input("Enter number :"))
-# result = sum_natural(n)
-# print("Sum is :",result)
-# # 5. Write a python function to print first n lines of the following pattern.
-# # ***
-# # **
-# # *
-# # - for n = 3
+n = int(input("Enter number :"))
+result = sum_natural(n)
+print("Sum is :",result)
 
-# def question5():
-#     n = 3
-#     for i in range(1,n+1):
-#         print("*" * (6 -(2 +i)))
 
-# question5()
+# 5. Write a python function to print first n lines of the following pattern.
+# ***
+# **
+# *
+# - for n = 3
 
-# # 6. Write a python function which converts inches to cms.
+def question5():
+    n = 3
+    for i in range(1,n+1):
+        print("*" * (6 -(2 +i)))
 
-# def inches_to_cm(inches):
-#     cm = inches * 2.54
-#     return cm
+question5()
 
-# inches = int(input("Enter inches :"))
-# result = inches_to_cm(inches)
-# print("Centimeter is :",result)
+# 6. Write a python function which converts inches to cms.
+
+def inches_to_cm(inches):
+    cm = inches * 2.54
+    return cm
+
+inches = int(input("Enter inches :"))
+result = inches_to_cm(inches)
+print("Centimeter is :",result)
 
 # 7. Write a python function to remove a given word from a list and strip it at the same time.
 
@@ -84,10 +87,10 @@ print(result)
 
 # 8. Write a python function to print multiplication table of a given number.
 
-# def multiplication(n):
-#     for i in range(1,11):
-#         print(n,"*",i,"=",n*i)
+def multiplication(n):
+    for i in range(1,11):
+        print(n,"*",i,"=",n*i)
 
-# num = int(input("Enter number :"))
-# multiplication(num)    
+num = int(input("Enter number :"))
+multiplication(num)    
 
