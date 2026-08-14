84f7be0569b3e02b81dc59583118b2cc3b147074
anjaleeyadav <yadavanjali101102@gmail.com>
2026-08-15 00:17:10 +0530

    Code added

--- 01-Python-Basics/Practice_Set_5.py
+++ 01-Python-Basics/Practice_Set_5.py
@@ -15,11 +15,11 @@ else:
 
 # 2. Write a program to input eight numbers from the user and display all the unique numbers (once).
 
-num = set()
-for i in range(8):
-    value = int(input(f"Enter number {i+1}:"))
-    num.add(value)
-print(num)
+# num = set()
+# for i in range(8):
+#     value = int(input(f"Enter number {i+1}:"))
+#     num.add(value)
+# print(num)
 
 
 # 3. Can we have a set with 18 (int) and '18' (str) as a value in it?
@@ -33,27 +33,30 @@ print(set)
 # s.add(20.0)
 # s.add('20') # length of s after these operations?
 
-s = set()
-s.add(20)  # 20 ==20.0 --> 20 ,both same in set
-s.add(20.0)
-s.add('20')
+# my_set = set()
+# my_set.add(20)  # 20 ==20.0 --> 20 ,both same in set
+# my_set.add(20.0)
+# my_set.add('20')
 
-print(s)
-print(len(s))
+# print(my_set)
+# print(len(my_set))
 
 # 5. s = {}
 # What is the type of 's'?
 
-s = {}
-print(type(s))
+s1 = {}
+print(type(s1))
 
 # 6. Create an empty dictionary. Allow 4 friends to enter their favorite language as value and use key as their names. Assume that the names are unique.
 
-# dictionary ={}
+friends ={}
 
-# for i in range(4):
-#     print("Enter your favourite language :")
-#     dictionary.add(i)
+for i in range(4):
+    name = input('Enter your name :')
+    language = input("Enter your favourite language :")
+    friends[name] = language 
+
+print(friends)
 
 
 # 7. If the names of 2 friends are same; what will happen to the program in problem 6?
