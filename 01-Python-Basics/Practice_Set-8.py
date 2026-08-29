46f593d918761dcee95602290f16c3e9c59af6f7
anjaleeyadav <yadavanjali101102@gmail.com>
2026-08-29 23:14:12 +0530

    updated code

--- 01-Python-Basics/Practice_Set-8.py
+++ 01-Python-Basics/Practice_Set-8.py
@@ -47,7 +47,36 @@ e.greet()
 # 5. Write a Class ‘Train’ which has methods to book a ticket, get status (no of seats) and get fare information of train 
 # running under Indian Railways.
 
+class Train:
+    def __init__(self, train_no, seats, fare):
+        self.train_no = train_no
+        self.seats = seats
+        self.fare = fare
 
+    def book_ticket(self):
+        if self.seats > 0:
+            self.seats -= 1
+            print("Ticket booked successfully!")
+        else:
+            print("Sorry, no seats available.")
+
+    def get_status(self):
+        print("Train Number:", self.train_no)
+        print("Available Seats:", self.seats)
+
+    def get_fare(self):
+        print("Fare per ticket: ₹", self.fare)
+
+
+# Creating object
+train = Train(12345, 5, 500)
+
+train.get_status()
+train.get_fare()
+
+train.book_ticket()
+
+train.get_status()
 
 
 # 6. Can you change the self-parameter inside a class to something else (say “harry”)? Try changing self to “slf” or “harry” 
